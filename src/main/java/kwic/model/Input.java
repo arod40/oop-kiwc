c5a7392726d6ea5941db659790d55ef58802f8c3
arod40 <arodacademic@gmail.com>
2023-01-31 12:23:31 -0600

    Implement Input filter

--- src/main/java/kwic/model/Input.java
+++ src/main/java/kwic/model/Input.java
@@ -1,6 +1,8 @@
 package kwic.model;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Input extends Filter{
   private FileInputStream fileInput;
@@ -8,5 +10,17 @@ public class Input extends Filter{
   public Input(FileInputStream input, Pipe output){
     super(null, output);
     this.fileInput = input;
+  }
+
+  @Override
+  public void run() {
+    Scanner scanner = new Scanner(this.fileInput);
+    while (scanner.hasNextLine()){
+      try {
+        output.writeLine(scanner.nextLine());
+      } catch (IOException e) {
+        throw new RuntimeException("Error while reading from input file");
+      }
+    }
   }
 }
