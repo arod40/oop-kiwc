df28062ad2f8d22c0a1fd09a31a103a1a52e877b
Austin Huizinga <42579548+austinth127@users.noreply.github.com>
2023-01-31 12:05:31 -0600

    Update Pipe.java

--- src/main/java/kwic/model/Pipe.java
+++ src/main/java/kwic/model/Pipe.java
@@ -1,11 +1,22 @@
 package kwic.model;
 
+import java.io.IOException;
 import java.io.PipedReader;
 import java.io.PipedWriter;
 
 public class Pipe {
   private PipedReader reader;
   private PipedWriter writer;
+
+  public Pipe() {
+    reader = new PipedReader();
+    writer = new PipedWriter();
+    try {
+      reader.connect(writer);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to connect pipes");
+    }
+  }
 
   public String readLine() {
     return "";
