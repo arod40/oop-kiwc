e09af9923139eec1cbf6a52f54c0668cf27e1912
arod40 <arodacademic@gmail.com>
2023-01-31 15:08:13 -0600

    Change Pipe to be closable and reimplement readLine with Scanner

--- src/main/java/kwic/model/Pipe.java
+++ src/main/java/kwic/model/Pipe.java
@@ -3,35 +3,29 @@ package kwic.model;
 import java.io.IOException;
 import java.io.PipedReader;
 import java.io.PipedWriter;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Scanner;
 
 public class Pipe {
-  private PipedReader reader;
-  private PipedWriter writer;
+  private PipedReader reader = new PipedReader();
+  private PipedWriter writer = new PipedWriter();
+  private Scanner scanner = new Scanner(reader);
 
   public Pipe() throws IOException {
-    this.reader = new PipedReader();
-    this.writer = new PipedWriter();
-
-    this.writer.connect(this.reader);
+    writer.connect(reader);
   }
 
-  public String readLine() throws IOException {
-    StringBuilder stringBuilder = new StringBuilder();
-
-    Character current = (char) reader.read();
-    while(current != '\n') {
-      stringBuilder.append(current);
-      current = (char) reader.read();
-    }
-
-    return stringBuilder.toString();
+  public String readLine(){
+    if (scanner.hasNextLine()) return scanner.nextLine();
+    return null;
   }
 
   public void writeLine(String line) throws IOException {
     for (char c : line.toCharArray())
       writer.write(c);
     writer.write('\n');
+  }
+
+  public void close() throws IOException {
+    writer.close();
   }
 }
